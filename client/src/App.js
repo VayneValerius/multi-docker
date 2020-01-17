9f1a3632a1cc7ab1d888374adb484ca491cab158
Joshua Kingswell <joshua.kingswell@rradar.com>
2020-01-17 11:30:22 +0000

    - Spelt containers wrong

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ function App() {
         {/* <header className="App-header"> */}
         <header>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1>Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
